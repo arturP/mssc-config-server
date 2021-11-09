fbd899361ac642f9d43a568e41ee7dd17f9ad7ef
Artur Przychodzen <artur2p@gmail.com>
2021-11-09 11:33:00 +0100

    init

---
+++ src/test/java/io/artur/spring/webservices/cloud/msscconfigserver/MsscConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.artur.spring.webservices.cloud.msscconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
